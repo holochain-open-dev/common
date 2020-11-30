a7eaca36efbbb0a68739b916c258f7c6811807a5
Guillem Cordoba <guillem.cordoba@gmail.com>
2020-11-30 17:13:13 +0100

    Updates

--- utils.js
+++ utils.js
@@ -13,4 +13,15 @@ export function cellIdToCommonMembrane(cellId) {
         },
     };
 }
+/**
+ * Setups the given element with the ApolloClient dependency
+ * The result is ready to call customElements.define()
+ */
+export function setupApolloClientElement(element, apolloClient) {
+    return class extends element {
+        get _apolloClient() {
+            return apolloClient;
+        }
+    };
+}
 //# sourceMappingURL=utils.js.map
