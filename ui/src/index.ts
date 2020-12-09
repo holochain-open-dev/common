614decae55c4876bc9415f964b99e4d6668574e0
Guillem Cordoba <guillem.cordoba@gmail.com>
2020-12-09 13:07:40 +0100

    Export from index

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -4,3 +4,4 @@ export { commonTypeDefs } from './graphql/schema';
 export { commonResolvers } from './graphql/resolvers';
 export * from './graphql/types';
 export * from './utils';
+export * as HolochainCoreTypes from './core-types';
