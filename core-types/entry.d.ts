a3da8a3b82ad0242744b025c068dea8de95b014d
Guillem Cordoba <guillem.cordoba@gmail.com>
2020-12-09 13:21:48 +0100

    Updates

--- core-types/entry.d.ts
+++ core-types/entry.d.ts
@@ -1,15 +1,13 @@
 import { CapClaim, ZomeCallCapGrant } from './capabilities';
 export declare type EntryVisibility = 'Public' | 'Private';
 export declare type AppEntryType = {
-    id: string;
+    id: number;
     zome_id: number;
     visibility: EntryVisibility;
 };
 export declare type EntryType = 'Agent' | {
     App: AppEntryType;
 } | 'CapClaim' | 'CapGrant';
-export declare function getAppEntryType(entryType: EntryType): AppEntryType | undefined;
-export declare function getEntryTypeString(entryType: EntryType): string;
 export interface EntryContent<E extends string, C> {
     entry_type: E;
     content: C;
--- core-types/entry.js
+++ core-types/entry.js
@@ -1,12 +1,1 @@
-export function getAppEntryType(entryType) {
-    if (entryType.App)
-        return entryType.App;
-    return undefined;
-}
-export function getEntryTypeString(entryType) {
-    const appEntryType = getAppEntryType(entryType);
-    if (appEntryType)
-        return appEntryType.id;
-    return entryType;
-}
 //# sourceMappingURL=entry.js.map
