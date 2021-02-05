7b4a4960d12885c05aea751ca777c03fc60ff906
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-02-05 13:27:49 +0100

    Updates

--- base-element.d.ts
+++ base-element.d.ts
@@ -1,8 +1,8 @@
-import { Constructor, LitElement } from 'lit-element';
+import { LitElement } from 'lit-element';
 import { Dictionary } from '@holochain-open-dev/core-types';
 declare const BaseElement_base: typeof LitElement & import("@open-wc/dedupe-mixin").Constructor<import("@open-wc/scoped-elements/types/src/types").ScopedElementsHost> & typeof import("@open-wc/scoped-elements/types/src/types").ScopedElementsHost;
 export declare class BaseElement extends BaseElement_base {
     connectedCallback(): void;
-    getScopedElements(): Dictionary<Constructor<HTMLElement>>;
+    getScopedElements(): Dictionary<typeof HTMLElement>;
 }
 export {};
