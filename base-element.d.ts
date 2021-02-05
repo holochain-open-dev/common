71a2635bcbe0ecc039920c5167392f85a5c6c354
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-02-05 13:25:33 +0100

    Updates

--- base-element.d.ts
+++ base-element.d.ts
@@ -1,13 +1,8 @@
 import { Constructor, LitElement } from 'lit-element';
 import { Dictionary } from '@holochain-open-dev/core-types';
 declare const BaseElement_base: typeof LitElement & import("@open-wc/dedupe-mixin").Constructor<import("@open-wc/scoped-elements/types/src/types").ScopedElementsHost> & typeof import("@open-wc/scoped-elements/types/src/types").ScopedElementsHost;
-export declare abstract class BaseElement<STORE> extends BaseElement_base {
+export declare class BaseElement extends BaseElement_base {
     connectedCallback(): void;
-    abstract get store(): STORE;
     getScopedElements(): Dictionary<Constructor<HTMLElement>>;
 }
-declare type AbstractConstructor<T> = Function & {
-    prototype: T;
-};
-export declare function connectStore<S, T extends AbstractConstructor<BaseElement<S>>>(baseClass: T, store: S): Constructor<HTMLElement>;
 export {};
--- base-element.js
+++ base-element.js
@@ -1,7 +1,6 @@
 import { LitElement } from 'lit-element';
 import { ScopedElementsMixin } from '@open-wc/scoped-elements';
-import { MobxReactionUpdate } from '@adobe/lit-mobx';
-export class BaseElement extends MobxReactionUpdate(ScopedElementsMixin(LitElement)) {
+export class BaseElement extends ScopedElementsMixin(LitElement) {
     connectedCallback() {
         super.connectedCallback();
         for (const [tag, el] of Object.entries(this.getScopedElements())) {
@@ -11,12 +10,5 @@ export class BaseElement extends MobxReactionUpdate(ScopedElementsMixin(LitEleme
     getScopedElements() {
         return {};
     }
-}
-export function connectStore(baseClass, store) {
-    return class extends baseClass {
-        get store() {
-            return store;
-        }
-    };
 }
 //# sourceMappingURL=base-element.js.map
