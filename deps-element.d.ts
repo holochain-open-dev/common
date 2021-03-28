5b9ec956c148f9a2c2edda59302ee31625923d23
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-03-28 12:56:03 +0200

    Updates

--- deps-element.d.ts
+++ deps-element.d.ts
@@ -1,11 +1,8 @@
 import { Constructor } from 'lit-element';
-import { BaseElement } from './base-element';
-declare const DepsElement_base: typeof BaseElement;
-export declare abstract class DepsElement<DEPENDENCIES> extends DepsElement_base {
-    abstract get deps(): DEPENDENCIES;
+export interface DepsElement<D> {
+    _deps: D;
 }
 export declare type AbstractConstructor<T> = Function & {
     prototype: T;
 };
 export declare function connectDeps<DEPS, T extends AbstractConstructor<DepsElement<DEPS>>>(baseClass: T, deps: DEPS): Constructor<HTMLElement>;
-export {};
--- deps-element.js
+++ deps-element.js
@@ -1,10 +1,6 @@
-import { MobxReactionUpdate } from '@adobe/lit-mobx';
-import { BaseElement } from './base-element';
-export class DepsElement extends MobxReactionUpdate(BaseElement) {
-}
 export function connectDeps(baseClass, deps) {
     return class extends baseClass {
-        get deps() {
+        get _deps() {
             return deps;
         }
     };
