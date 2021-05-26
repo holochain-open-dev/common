7176a2b21ffaa9a6ac38c506bbd46d71452adc46
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-05-26 17:51:42 +0200

    Updates

--- deps-element.d.ts
+++
@@ -1,8 +0,0 @@
-import { Constructor } from 'lit-element';
-export interface DepsElement<D> {
-    _deps: D;
-}
-export declare type AbstractConstructor<T> = Function & {
-    prototype: T;
-};
-export declare function connectDeps<DEPS, T extends AbstractConstructor<DepsElement<DEPS>>>(baseClass: T, deps: DEPS): Constructor<HTMLElement>;
--- deps-element.js
+++
@@ -1,8 +0,0 @@
-export function connectDeps(baseClass, deps) {
-    return class extends baseClass {
-        get _deps() {
-            return deps;
-        }
-    };
-}
-//# sourceMappingURL=deps-element.js.map
