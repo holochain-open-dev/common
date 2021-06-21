46765e624bd55531f11768dca8a94806865b4b6a
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-06-21 11:26:24 +0200

    Fixed contextprovider type

--- ui/src/context-provider-element.ts
+++ ui/src/context-provider-element.ts
@@ -3,27 +3,25 @@ import { property } from 'lit/decorators.js';
 import { ContextTypeMap } from './context-event';
 import { ContextProvider } from './context-provider';
 
-export class ContextProviderElement<
-  T extends keyof ContextTypeMap
-> extends LitElement {
-  private localValue?: ContextTypeMap[T];
+export class ContextProviderElement extends LitElement {
+  private localValue?: any;
 
   @property({ attribute: false })
-  public set value(value: ContextTypeMap[T]) {
+  public set value(value: any) {
     this.localValue = value;
     if (this.context) {
-      this.context.value = value;
+      this.context.value = value as never;
     }
   }
 
-  private context?: ContextProvider<T>;
+  private context?: ContextProvider<never>;
 
   @property()
   public set name(value: string) {
     if (!this.context) {
-      this.context = new ContextProvider(this, value as T);
+      this.context = new ContextProvider(this, value as never);
       if (this.localValue) {
-        this.context.value = this.localValue;
+        this.context.value = this.localValue as never;
       }
     } else {
       throw new Error('Can only set context provider element name once!');
